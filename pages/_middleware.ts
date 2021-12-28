9e9847944c1a5ea1e48a7e2c19b24190c6a90021
DongKyu <po4tion0429@gmail.com>
2021-12-28 12:52:29 +0900

    Fix: secureCookie

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -7,10 +7,19 @@ import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextApiRequest) {
-	const token = await getToken({ req, secret: process.env.JWT_SECRET });
+	const token = await getToken({
+		req,
+		secret: process.env.JWT_SECRET,
+		secureCookie:
+			process.env.NEXTAUTH_URL?.startsWith('https://') ??
+			!!process.env.VERCEL_URL,
+	});
 
 	const { url: pathname } = req;
 
+	if (token && pathname === '/login') {
+		return NextResponse.redirect('/');
+	}
 	// url이 api/auth 경로를 포함하고 있거나 token(로그인 성공)
 	// 값을 가지고 있다면 미들웨어 체인을 계속 진행
 	if (pathname?.includes('/api/auth') || token) {
@@ -18,7 +27,7 @@ export async function middleware(req: NextApiRequest) {
 	}
 
 	// url이 '/login'도 아니고 token 값도 없다면 로그인 페이지로 redirect
-	if (pathname !== '/login' && !token) {
+	if (!token && pathname !== '/login') {
 		return NextResponse.redirect('/login');
 	}
 }
